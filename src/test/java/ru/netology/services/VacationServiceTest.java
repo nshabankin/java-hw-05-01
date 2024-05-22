7bdd05bbd6c2bed0aa79dd58c468af77e7e746a8
nshabankin <n.shabankin@gmail.com>
2024-05-22 21:30:36 +0300

    Added parameterized tests with .csv source

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,28 +1,18 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void testVacationsCount1() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/parameters.csv")
+    public void testVacationsCount(int expected, int income, int expenses, int threshold) {
 
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testVacationsCount2() {
-
-        VacationService service = new VacationService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
